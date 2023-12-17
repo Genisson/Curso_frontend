fc956646ed35a034b51dd9344d6e3abbe89fbfb4
Gênisson de Freitas <genissonfg@gmail.com>
2023-12-17 12:11:29 -0300

    Aula 06 - Var Let Const - Variáveis

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>!";
+
+let x = 10; //aqui é 10
+
+{
+
+    let x = 2; // arqui é 2
+
+    document.getElementById("texto").innerHTML = x;
+}
+
+// aqui fora do bloco irá pegar o valor de fora também que é 10.
+//document.getElementById("texto").innerHTML = x;
